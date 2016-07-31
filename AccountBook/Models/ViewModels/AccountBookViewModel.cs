1c73a74b57e2c82c4697ad413c52ef37ac77cab3
LoopsLu <beethovenlu@hotmail.com>
2016-07-31 16:06:22 +0800

    Index頁面加入金額、日期、備註欄位，以及送出鈕

---
+++ AccountBook/Models/ViewModels/AccountBookViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountBook.Models.ViewModels
+{
+    public enum CategoryEnum
+    {
+        [Description("支出")]
+        Expense, 
+        [Description("收入")]
+        Income
+    }
+    public class AccountBookViewModel
+    {
+        [Display(Name = "類別")]
+        public CategoryEnum CategoryList { get; set; }
+
+        [Display(Name = "金額")]
+        public decimal Value { get; set; }
+
+        [Display(Name = "日期")]
+        public DateTime DateTime { get; set; }
+
+        [Display(Name = "備註")]
+        public string Comment { get; set; }
+    }
+}

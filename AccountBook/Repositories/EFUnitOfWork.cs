8cec54c391dd5113ac4763be6d52dc8ae84c53b3
LoopsLu <beethovenlu@hotmail.com>
2016-08-07 11:11:47 +0800

    新增IUnitOfWork並實作出EFUnitOfWork

---
+++ AccountBook/Repositories/EFUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using AccountBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AccountBook.Repositories
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; set; }
+
+        public EFUnitOfWork()
+        {
+            Context = new AccountBookRecordModel();
+        }        
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
---
+++ AccountBook/Repositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountBook.Repositories
+{
+    interface IUnitOfWork : IDisposable
+    {
+        DbContext Context { get; set; }
+    }
+}
